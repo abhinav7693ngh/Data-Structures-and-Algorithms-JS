13423a2125a075f62b1dbeb3a90a39e017d36763
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-02-19 19:05:11 +0530

    Binary Search

---
+++ Searching Algorithms/BinarySearch_ATV.js
@@ -0,0 +1,21 @@
+function binarySearch(arr,me){
+    var left = 0;
+    var right = arr.length -1;
+    var middle = Math.floor((left+right)/2);
+    console.log(left,right,middle);
+    while(arr[middle]!=me){
+        if(left>right){
+            return -1;
+        }
+        if(me  < arr[middle]){
+            right = middle-1;
+        }
+        else{
+            left = middle+1;
+        }
+        middle = Math.floor((left+right)/2);
+    }
+    return middle;
+}
+const myarray = [1,2,3,4,5,6,7];
+console.log(binarySearch(myarray,7));
