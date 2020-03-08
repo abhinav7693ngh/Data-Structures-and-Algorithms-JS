6d07d3aa680237f40fffb9d1b6d6c86f4af9c4c6
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-08 16:26:25 +0530

    class syntax

---
+++ Data Structures/class_syntax.js
@@ -0,0 +1,10 @@
+class Student{
+    constructor(firstName, lastName, year){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = year;
+    }
+}
+
+const firstStudent = new Student('Abhinav','Singh',4);
+console.log(firstStudent);
