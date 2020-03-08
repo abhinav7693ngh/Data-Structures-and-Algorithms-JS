73852a63d77364f4aba62cf07fd10a2c2ff1faac
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-08 16:39:20 +0530

    instance and class methods

--- Data Structures/class_syntax.js
+++ Data Structures/class_syntax.js
@@ -4,7 +4,24 @@ class Student{
         this.lastName = lastName;
         this.grade = year;
     }
+
+    fullName(){
+        console.log(`Your Full name is ${this.firstName} ${this.lastName}`);
+        console.log('This is my instance method');
+    }
+
+    yetAnother = () => {
+        console.log(`From another instance method(but this act like property not method) ${this.myproperty}`); 
+    }
+
+    myproperty = 'Some random testing property';
+
+    static myfunc(){
+        console.log('So this is my class methods');
+    }
 }
 
 const firstStudent = new Student('Abhinav','Singh',4);
+console.log(firstStudent.yetAnother());
+console.log(Student.myfunc());
 console.log(firstStudent);
