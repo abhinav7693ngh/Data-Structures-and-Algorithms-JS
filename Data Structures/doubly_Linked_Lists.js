83836f7d5d252f27c0c0c66ce4e3621293cf11c2
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-09 20:49:37 +0530

    shift() method doubly linked list

--- Data Structures/doubly_Linked_Lists.js
+++ Data Structures/doubly_Linked_Lists.js
@@ -34,15 +34,30 @@ class DoublyLinkedList{
         else if(this.length == 1){
             this.head = null;
             this.tail = null;
+            --this.length;
         }
         else{
             let newLast = this.tail.prev;
             newLast.next = null;
             this.tail = newLast;
+            --this.length;
         }
-        --this.length;
     }
     shift(){
+        if(this.length == 0){
+            console.log('Empty');
+        }
+        else if(this.length == 1){
+            this.head = null;
+            this.tail = null;
+            --this.length;
+        }
+        else{
+            let newStart = this.head.next;
+            newStart.prev = null;
+            this.head = newStart;
+            --this.length;
+        }
     }
 }
 
@@ -50,5 +65,9 @@ const list = new DoublyLinkedList();
 list.push(10);
 list.push(20);
 list.push(30);
+list.push(40);
+list.push(50);
+list.push(60);
 list.pop();
+list.shift();
 console.log(list);
