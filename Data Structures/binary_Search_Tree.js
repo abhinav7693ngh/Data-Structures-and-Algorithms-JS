03f023c5ecbd976e3e1398c27a62dd528552acc1
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-10 17:25:16 +0530

    BST insert() and find() method

--- Data Structures/binary_Search_Tree.js
+++ Data Structures/binary_Search_Tree.js
@@ -10,7 +10,86 @@ class BinarySearchTree{
     constructor() {
         this.root = null;
     }
+    insert(val){
+        let newNode = new Node(val);
+        if(this.root == null){
+            this.root = newNode;
+        }
+        else{
+            let trav = this.root;
+            while(true){
+                if(newNode.value <= trav.value){
+                    if(trav.left != null){
+                        trav = trav.left;
+                    }
+                    else{
+                        trav.left = newNode;
+                        break;
+                    }
+                }
+                else{
+                    if(newNode.value > trav.value){
+                        if(trav.right != null){
+                            trav = trav.right;
+                        }
+                        else{
+                            trav.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    find(my){
+        if(this.root == null){
+            console.log('Tree Empty');
+        }
+        else{
+            let trav = this.root;
+            while(true){
+                console.log(trav);
+                if(my == trav.value){
+                    console.log(trav);
+                    break;
+                }
+                else if(my <= trav.value && trav.left!=null){
+                    trav = trav.left;
+                }
+                else if(my > trav.value && trav.right!=null){
+                    trav = trav.right;
+                }
+                else{
+                    console.log('Not Found');
+                    break;
+                }
+            }
+        }
+    }
 }
+
+let tree =  new BinarySearchTree();
+tree.insert(50);
+tree.insert(40);
+tree.insert(70);
+tree.insert(20);
+tree.insert(45);
+tree.insert(60);
+tree.insert(80);
+tree.find(100);
+console.log(tree);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
