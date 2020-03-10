a62f9c5df66f343e95e150e53181d4630bfca53f
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-10 16:00:13 +0530

    Random code play BST

---
+++ Data Structures/binary_Search_Tree.js
@@ -0,0 +1,37 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor() {
+        this.root = null;
+    }
+}
+
+
+
+
+
+
+
+
+// ===== Random code play making Binary Search tree ===== //
+
+
+/*
+const tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(5);
+tree.root.right.right = new Node(17);
+tree.root.right.left = new Node(13);
+tree.root.left.right = new Node(8);
+tree.root.left.left = new Node(3);
+console.log(tree);
+*/
+
+// ===== End ===== //
