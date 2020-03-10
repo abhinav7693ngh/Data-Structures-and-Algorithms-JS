7f6f7156f3dc58524231cc85da15e018d8a2bee2
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-11 00:27:45 +0530

    Max_Binary_Heap extractMax() method

--- Data Structures/Binary_heap.js
+++ Data Structures/Binary_heap.js
@@ -15,7 +15,58 @@ class MaxBinaryHeap{
             valueIndex = parentIndex;
             parentIndex = Math.floor((valueIndex-1)/2);
         }
+    }
+    extractMax(){
         console.log(this.values);
+        if(this.values.length == 0){
+            console.log('Empty');
+        }
+        else if(this.values.length == 1){
+            return this.values.pop();
+        }
+        else if(this.values.length ==  2){
+            return this.values.shift();
+        }
+        else if(this.values.length == 3){
+            let max = this.values.shift();
+            if(this.values[0]<this.values[1]){
+                let temp = this.values[1];
+                this.values[1] = this.values[0];
+                this.values[0] = temp;
+            }
+            console.log(this.values);
+            return max;
+        }
+        else{
+            let temp = this.values[0];
+            this.values[0] = this.values[this.values.length-1];
+            this.values[this.values.length - 1] = temp;
+            const max = this.values.pop();
+            let index = 0;
+            let left = this.values[2*index + 1];
+            let right = this.values[2*index + 2];
+            while(this.values[index]<left || this.values[index]<right){
+                if(right<left){
+                    let temp2 = this.values[index];
+                    this.values[index] = this.values[2*index + 1];
+                    this.values[2*index + 1] = temp2;
+                    index = 2*index + 1;
+                    left = this.values[2*index + 1];
+                    right = this.values[2*index + 2];
+                }
+                else{
+                    let temp2 = this.values[index];
+                    this.values[index] = this.values[2*index + 2];
+                    this.values[2*index + 2] = temp2; 
+                    index = 2*index + 2;
+                    left = this.values[2*index + 1];
+                    right = this.values[2*index + 2];
+                }
+            }
+            console.log(this.values);
+            return max;
+        }
+        
     }
 }
 
@@ -27,3 +78,11 @@ maxHeap.insert(18);
 maxHeap.insert(27);
 maxHeap.insert(12);
 maxHeap.insert(55);
+console.log(maxHeap.extractMax());
+console.log(maxHeap.extractMax());
+console.log(maxHeap.extractMax());
+console.log(maxHeap.extractMax());
+console.log(maxHeap.extractMax());
+console.log(maxHeap.extractMax());
+console.log(maxHeap.extractMax());
+console.log(maxHeap.extractMax());
