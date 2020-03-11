0f56c0c87d7bee99379478a5b8d36117dda184d5
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-11 22:21:27 +0530

    Undirected Graph removeVertex() method

--- Data Structures/Graph_Undirected.js
+++ Data Structures/Graph_Undirected.js
@@ -19,9 +19,24 @@ class Graph{
     removeEdge(vertex1,vertex2){
         if(this.adjancencyList[vertex1] && this.adjancencyList[vertex2]){
             const index21 = this.adjancencyList[vertex1].findIndex(element => element == vertex2);
-            this.adjancencyList[vertex1].splice(index21,1);
+            if(index21!=-1){
+                this.adjancencyList[vertex1].splice(index21, 1);
+            }
             const index12 = this.adjancencyList[vertex2].findIndex(element => element == vertex1);
-            this.adjancencyList[vertex2].splice(index12,1);
+            if(index12!=-1){
+                this.adjancencyList[vertex2].splice(index12, 1);
+            }
+        }
+        else{
+            console.log('Please enter a valid vertex');
+        }
+    }
+    removeVertex(vertex){
+        if(this.adjancencyList[vertex]){
+            for(let x in this.adjancencyList){
+                this.removeEdge(x,vertex);
+            }
+            delete this.adjancencyList[vertex];
         }
         else{
             console.log('Please enter a valid vertex');
@@ -37,6 +52,11 @@ myGraph.addVertex("C");
 myGraph.addVertex("D");
 myGraph.addVertex("E");
 myGraph.addVertex("F");
-myGraph.addEdge("A","B");
-myGraph.removeEdge("A","B");
+myGraph.addEdge("A", "B");
+myGraph.addEdge("B", "C");
+myGraph.addEdge("C", "D");
+myGraph.addEdge("D", "E");
+myGraph.addEdge("E", "F");
+myGraph.addEdge("F", "A");
+myGraph.removeVertex("A");
 console.log(myGraph);
