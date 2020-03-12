edeb60eafa0842a3d0dcf4045b3cf6aaf1ac450d
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-12 14:08:31 +0530

    Weighted graph removeEdge() method

--- Data Structures/Dijkstra_Algorithm.js
+++ Data Structures/Dijkstra_Algorithm.js
@@ -1,4 +1,4 @@
-console.log('Implementation of Dijkstra\'s Algorithm');
+console.log('Implementation of Dijkstra\'s Algorithm and weighted graph');
 
 class WeightedGraph{
     constructor() {
@@ -18,12 +18,38 @@ class WeightedGraph{
             console.log('Please enter a valid node');
         }
     }
+    removeEdge(vertex1,vertex2){
+        if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+            let index12 = this.adjacencyList[vertex1].findIndex(ele => ele.node == vertex2);
+            if(index12!=-1){
+                this.adjacencyList[vertex1].splice(index12,1);
+            }
+            let index21 = this.adjacencyList[vertex2].findIndex(ele => ele.node == vertex1);
+            if(index21!=-1){
+                this.adjacencyList[vertex2].splice(index21,1);
+            }
+        }
+        else{
+            console.log('Please enter a valid edge');
+        }
+    }
 }
 
 let myGraph = new WeightedGraph();
 myGraph.addVertex("A");
 myGraph.addVertex("B");
-myGraph.addEdge("A","B",10);
+myGraph.addVertex("C");
+myGraph.addVertex("D");
+myGraph.addVertex("E");
+myGraph.addVertex("F");
+myGraph.addEdge("A", "B",10);
+myGraph.addEdge("B", "D",20);
+myGraph.addEdge("D", "E",15);
+myGraph.addEdge("D", "F",5);
+myGraph.addEdge("F", "E",20);
+myGraph.addEdge("E", "C",30);
+myGraph.addEdge("C", "A",6);
+myGraph.removeEdge("A","B");
 console.log(myGraph);
 
 
