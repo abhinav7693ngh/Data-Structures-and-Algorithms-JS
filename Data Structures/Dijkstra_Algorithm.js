1b496c85ce74c622d57034ae0d68c2db166b5755
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-12 14:01:49 +0530

    Weighted graph addVertex() and addEdge() methiod

--- Data Structures/Dijkstra_Algorithm.js
+++ Data Structures/Dijkstra_Algorithm.js
@@ -8,13 +8,22 @@ class WeightedGraph{
         if(!this.adjacencyList[vertex]){
             this.adjacencyList[vertex] = [];
         }
+    }   
+    addEdge(vertex1,vertex2,weight){
+        if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+            this.adjacencyList[vertex1].push({node : vertex2, weight : weight});
+            this.adjacencyList[vertex2].push({node : vertex1, weight : weight});
+        }
+        else{
+            console.log('Please enter a valid node');
+        }
     }
-    
 }
 
 let myGraph = new WeightedGraph();
 myGraph.addVertex("A");
 myGraph.addVertex("B");
+myGraph.addEdge("A","B",10);
 console.log(myGraph);
 
 
